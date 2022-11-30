acf97488a5d4ea30cf6d4c5ab42bce439ae15d66
Antoine <antoinevernaire@live.fr>
2022-11-30 11:14:22 -0500

    Gros changements

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,40 @@
 #include <Arduino.h>
 #include "secrets.h"
 #include "WiFi.h"
+#include <PubSubClient.h>
+#include <NTPClient.h>
 
+// Nom d'utilisateur et Mot de passe WiFi
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
 
 const char *host = "10.20.60.74";
-const int port = 10000;
+const int port = 3325;
+
+// --Serveur MTTQ--
+// const char server[] = {"cmaisonneuve.cloud.shiftr.io"};
+const char server[] = {"broker.hivemq.com"};
+
+// --Nom d'utilisateur--
+// const char user[] = {"cmaisonneuve"};
+
+// --Mot de passe--
+// const char pass[] = {"FmIOfnkb8BOZaUzw"};
+const char payload[] = {"200,125,156"};
+
+// --Initiation objet Cilents WiFi et MQTT--
+WiFiClient Client_wifi;
+PubSubClient ClientMQTT(server, 1883, Client_wifi);
+
+// --Initiation variable timer--
+int lastNow = 0;
+
+const char *str_inp1 = "/cm/bipeds/1";
+const char *str_inp2 = "/cm/bipeds/2";
+const char *str_inp3 = "/cm/bipeds/3";
+const char *str_inp4 = "/cm/bipeds/4";
+const char *str_inp5 = "/cm/bipeds/5";
+const char *str_inp6 = "/cm/bipeds/6";
 
 String translateEncryptionType(wifi_auth_mode_t encryptionType)
 {
@@ -28,32 +56,27 @@ String translateEncryptionType(wifi_auth_mode_t encryptionType)
     return "WPA_WPA2_PSK";
   case (WIFI_AUTH_WPA2_ENTERPRISE):
     return "WPA2_ENTERPRISE";
-  default:  
+  default:
     return "UNKNOWN";
   }
   return "UNKNOWN";
 }
 
+// --Affichage des reseaux disponiles--
 void scanNetworks()
 {
-
   int numberOfNetworks = WiFi.scanNetworks();
-
   Serial.print("Number of networks found: ");
   Serial.println(numberOfNetworks);
 
   for (int i = 0; i < numberOfNetworks; i++)
   {
-
     Serial.print("Network name: ");
     Serial.println(WiFi.SSID(i));
-
     Serial.print("Signal strength: ");
     Serial.println(WiFi.RSSI(i));
-
     Serial.print("MAC address: ");
     Serial.println(WiFi.BSSIDstr(i));
-
     Serial.print("Encryption type: ");
     String encryptionTypeDescription = translateEncryptionType(WiFi.encryptionType(i));
     Serial.println(encryptionTypeDescription);
@@ -61,6 +84,7 @@ void scanNetworks()
   }
 }
 
+// --Connection au reseau choisi--
 void connectToNetwork()
 {
   WiFi.begin(ssid, password);
@@ -74,32 +98,119 @@ void connectToNetwork()
   Serial.println("Connected to network");
 }
 
-void sendMessageToTCPServer() {
-  Serial.println("Send TCP message to server");
-  WiFiClient client;
-  if (!client.connect(host, port))
+// --Fonction de retour de donne a l'abonnement
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  float angle;
+  angle = atof ((const char *)payload);
+  Serial.printf("%4.2f\n\r", angle);
+  uint32_t temps_haut = (angle + 80) * 10 / 160; 
+
+  if (strcmp(str_inp1, topic) == 0)
   {
-    Serial.println("connection failed");
-    return;
+    Serial.printf("%s\n\r", payload);
+    Serial.printf("%u\n\r", (uint32_t)payload*2);
+    // --Changement d'etat de la LED selon le message recu--
+    // Temps_haut = payload
+    // ledcWrite(1, 77);
   }
-  client.print("My name is ESP32-Teacher");
-  client.stop();
+  else if (strcmp(str_inp2, topic) == 0)
+  {
+    // --Changement d'etat de la LED selon le message recu--
+    Serial.printf("%s\n\r", payload);
+    // Temps_haut = payload
+    // ledcWrite(1, 77);
+  }
+  else if (strcmp(str_inp3, topic) == 0)
+  {
+    // --Changement d'etat de la LED selon le message recu--
+    Serial.printf("%s\n\r", payload);
+    // Temps_haut = payload
+    // ledcWrite(1, 77);
+  }
+  else if (strcmp(str_inp4, topic) == 0)
+  {
+    // --Changement d'etat de la LED selon le message recu--
+    Serial.printf("Moteur 4\n\r");
+    // Temps_haut = payload
+    // ledcWrite(1, 77);
+  }
+  else if (strcmp(str_inp5, topic) == 0)
+  {
+    // --Changement d'etat de la LED selon le message recu--
+    Serial.printf("Moteur 5\n\r");
+    // Temps_haut = payload
+    // ledcWrite(1, 77);
+  }
+  else if (strcmp(str_inp6, topic) == 0)
+  {
+    // --Changement d'etat de la LED selon le message recu--
+    Serial.printf("Moteur 6\n\r");
+    // Temps_haut = payload
+    // ledcWrite(1, 77);
+  }
 }
 
+// --Initiation des composants necessaires--
 void setup()
 {
-
+  // --Port serie--
   Serial.begin(115200);
 
+  // --Moteur1--
+  pinMode(5, OUTPUT);
+  // --Moteur2--
+  pinMode(18, OUTPUT);
+  // --Moteur3--
+  pinMode(19, OUTPUT);
+  // --Moteur4--
+  pinMode(16, OUTPUT);
+  // --Moteur5--
+  pinMode(17, OUTPUT);
+  // --Moteur6--
+  pinMode(21, OUTPUT);
+
+  // Configurer fonction du PWM
+  ledcSetup(1, 50, 10);
+  ledcSetup(2, 50, 10);
+  ledcSetup(3, 50, 10);
+  ledcSetup(4, 50, 10);
+  ledcSetup(5, 50, 10);
+  ledcSetup(6, 50, 10);
+
+  // Attache le canal au GPIO pour etre controller
+  ledcAttachPin(5, 1);
+  ledcAttachPin(18, 2);
+  ledcAttachPin(19, 3);
+  ledcAttachPin(16, 4);
+  ledcAttachPin(17, 5);
+  ledcAttachPin(21, 6);
+
+  // --Pointeur fonction de retour--
+  ClientMQTT.setCallback(callback);
+
+  // --Connection WiFi--
   scanNetworks();
   connectToNetwork();
 
+  // --Affichage caracteristiques WiFi--
   Serial.println(WiFi.macAddress());
   Serial.println(WiFi.localIP());
+
+  // --Connection au client MQTT--
+  // ClientMQTT.connect("client-id-maisonneuve", user, pass);
+  ClientMQTT.connect("client-id-69");
+
+  // --Abonnement au sujet--
+  ClientMQTT.subscribe("/cm/bipeds/#");
 }
 
+// --Boucle principale--
 void loop()
 {
-  sendMessageToTCPServer();
-  delay(2000);
+  if (!ClientMQTT.connected())
+  {
+    Serial.printf("Client MQTT deconnecte\n\r");
+  }
+  ClientMQTT.loop();
 }
