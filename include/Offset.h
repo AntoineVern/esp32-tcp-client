4655bc1a8c8e0251bedaaf9ecadb4cc34ddea9ca
Antoine <antoinevernaire@live.fr>
2022-12-06 12:42:44 -0500

    Commencement de la création des offsets

---
+++ include/Offset.h
@@ -0,0 +1,32 @@
+#define Judith 1        //pas plus que 45
+#define Balthazar 2
+#define Igor 3
+#define Karen 4
+#define Gregory 5
+#define Jennyfer 6
+
+float offset(int name){
+if (name == Judith){
+
+}
+if (name == Balthazar){
+  /*ledcWrite(1, 80); //off +3.2
+  ledcWrite(2, 76.8); 
+  ledcWrite(3, 76.8);
+  ledcWrite(4, 76.8);
+  ledcWrite(5, 76.8); 
+  ledcWrite(6, 78); //off +1.2 */
+}
+if (name == Igor){
+    
+}
+if (name == Karen){
+    
+}
+if (name == Gregory){
+    
+}
+if (name == Jennyfer){
+    
+}
+}
